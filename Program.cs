5036086833f9b3a6a87b73bace17ba09daf525c8
julia shavrina <tifanys@yandex.ru>
2022-07-26 16:30:25 +0300

    Homework9

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая найдет
+ кол-во цифр в числе N рекурсивным методом. */
+/*int ShowCountOfNumber ( int n)
+{
+    if ( n / 10 > 0)
+    {
+        return 1 + ShowCountOfNumber (n/10);
+    }
+    else return 1;
+}
+Console.WriteLine (ShowCountOfNumber(123));
+*/
+
+//Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*int SumofDigits (int m,int n)
+{
+    if (n > m)
+    {
+        return m + SumofDigits (m+1,n);
+    }
+        else return m;
+    
+}
+Console.WriteLine(SumofDigits (1,15));
+*/
